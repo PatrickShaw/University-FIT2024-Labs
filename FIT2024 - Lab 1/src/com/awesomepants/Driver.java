fdff8d0208df930b0a0d24f8030aeedd0bba439b
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2016-04-05 16:03:32 +1000

    FIT2024 Lab 2

---
+++ FIT2024 - Lab 1/src/com/awesomepants/Driver.java
@@ -0,0 +1,11 @@
+package com.awesomepants;
+
+/**
+ * Created by eastd on 8/03/2016.
+ */
+public class Driver {
+    public static void main(String[] args) {
+        University monash = new University();
+        monash.run();
+    }
+}
